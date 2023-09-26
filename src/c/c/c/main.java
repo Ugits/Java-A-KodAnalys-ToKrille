68c0d06ded5df9c83012463ca83d68a116a80be4
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-26 14:43:38 +0200

    first commit

---
+++ src/c/c/c/main.java
@@ -0,0 +1,27 @@
+package c.c.c;
+
+public class main {
+    public static void main(String[] args) {
+
+        /* TODO
+         *   Change the names of files + packages to relevant names
+         *   Find the errors in the code and resolve it!
+         */
+         */
+         * */
+
+        for (int I = 0; i < 5,i++){
+            system.out.println("Inside the method runMyForLoop" + i)
+        }
+    }
+
+    student student = new student("") {
+        student.sayHello();
+    }
+
+    }
+}
+
+public static void runMyForLoop() {
+
+        }
